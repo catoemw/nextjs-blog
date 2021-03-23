55562df173a87d73d3fb312855d6cbad25ca7a65
Matt Catoe <catoemw@gmail.com>
2021-03-22 21:26:03 -0400

    Add CSS module

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,7 @@
-const Layout = ({ children }) => <Wrapper>{children}</Wrapper>;
+import styles from "./layout.module.css";
+
+const Layout = ({ children }) => (
+  <div className={styles.container}>{children}</div>
+);
 
 export default Layout;
